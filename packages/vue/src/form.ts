52ec3874b9eff5975e9e020fb51b9ac8673b655b
Trilmatic <suchanek-petr@post.cz>
2024-02-09 20:36:52 +0100

    create base vue package and update packages dependency

--- packages/vue/src/form.ts
+++ packages/vue/src/form.ts
@@ -3,46 +3,46 @@ import { Form as FormConstructor, PluginSettings, usePlugin } from '@forms.js/co
 import { PropType, defineComponent, h } from 'vue';
 
 const Form = defineComponent({
-    props: {
-        options: {
-            type: Object as PropType<FormOptions>,
-            required: true
-        }
+  props: {
+    options: {
+      type: Object as PropType<FormOptions>,
+      required: true,
     },
+  },
 
-    data() {
-        return {
-            formInstance: null as FormConstructor | null
-        }
+  data() {
+    return {
+      formInstance: null as FormConstructor | null,
+    };
+  },
+
+  methods: {
+    getForm(): FormConstructor | null {
+      return this.formInstance as FormConstructor | null;
     },
-
-    methods: {
-        getForm(): FormConstructor | null {
-            return this.formInstance as FormConstructor | null;
-        },
-        usePlugin(pluginSettings: PluginSettings | PluginSettings[]) {
-            usePlugin(pluginSettings);
-        }
+    usePlugin(pluginSettings: PluginSettings | PluginSettings[]) {
+      usePlugin(pluginSettings);
     },
+  },
 
-    render() {
-        return h('div', {
-            attrs: { 'data-formsjs-id': this.$props.options.id }
-        })
-    },
+  render() {
+    return h('div', {
+      attrs: { 'data-formsjs-id': this.$props.options.id },
+    });
+  },
 
-    mounted() {
-        this.formInstance = new FormConstructor(this.$el as HTMLElement, this.$props.options);
-    },
+  mounted() {
+    this.formInstance = new FormConstructor(this.$el as HTMLElement, this.$props.options);
+  },
 
-    beforeUnmount() {
-        const form = this.getForm();
-        form.destroy();
-    },
+  beforeUnmount() {
+    const form = this.getForm();
+    form?.destroy();
+  },
 
-    watch: {
-        // check for option changes and rerender form
-    },
-})
+  watch: {
+    // check for option changes and rerender form
+  },
+});
 
-export default Form
+export default Form;
