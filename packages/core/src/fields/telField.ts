33df52f17c5b97ca3c644edd8f3898f0740bc92e
Trilmatic <suchanek-petr@post.cz>
2024-06-27 21:34:22 +0200

    run format and lint

--- packages/core/src/fields/telField.ts
+++ packages/core/src/fields/telField.ts
@@ -23,7 +23,7 @@ export class TelField extends Field {
   };
 
   public telFormat = /^\+?\d{1,3}?[-.\s]?\(?\d{1,4}?\)?([-.\s]?\d{1,9})*$/;
-  
+
   constructor(parent: HTMLElement, form: Form, options: FieldOptions) {
     super(parent, form, options);
     this.initializeOptions(options);
